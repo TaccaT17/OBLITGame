44e18a5877bf1ed15d395e83f2b014b7bb408048
TaccaT17 <tom@crammond.name>
2021-02-07 12:36:10 -0800

    UnReal Project made

---
+++ Source/OBLIT/OBLIT.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OBLIT.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OBLIT, "OBLIT" );
---
+++ Source/OBLIT/OBLITGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OBLITGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OBLIT_API AOBLITGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
